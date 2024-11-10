52dd310e47e0b3fab512c0febd215ae10d587d7b
Ishan Ghosh <ishanghosh@mac.lan>
2024-11-10 12:05:55 -0800

    stitched everything togeter and seperated them into folders

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from callGPT import call_chatgpt
-import stt
+from GPT.callGPT import call_chatgpt
+import sound.stt as stt
+from sound.tts import TTS
 
 result = call_chatgpt(stt.text)
-print(result)
+TTS(result)
